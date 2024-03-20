4c28f2f1af77adb05f1569e6152448a51b430ad7
SalmaMiha <salma.islam.1999@gmail.com>
2024-03-20 07:49:23 +0600

    section title style and category section added

--- src/Components/SectionTitle/SectionTitle.jsx
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -1,8 +1,8 @@
 const SectionTitle = ({ heading, subHeading }) => {
   return (
-    <div>
-      <p>{subHeading}</p>
-      <h2>{heading}</h2>
+    <div className="mx-auto md:w-3/12 my-8 text-center">
+      <p className="text-yellow-600 mb-2">--- {subHeading} ---</p>
+      <h2 className="text-3xl uppercase border-y-2 py-2">{heading}</h2>
     </div>
   );
 };
