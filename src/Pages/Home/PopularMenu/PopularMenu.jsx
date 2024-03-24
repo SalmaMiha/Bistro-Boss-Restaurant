5fc8f2b0edfb60fc83f2a2c23ef1eb18e19e9bcb
SalmaMiha <salma.islam.1999@gmail.com>
2024-03-24 16:30:30 +0600

    testimonials and popular menu button added

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -18,10 +18,15 @@ const PopularMenu = () => {
         heading={"From our menu"}
         subHeading={"Popular Menu"}
       ></SectionTitle>
-      <div className="grid md:grid-cols-2 gap-4">
-        {menu.map((item) => (
-          <MenuItem key={item._id} item={item}></MenuItem>
-        ))}
+      <div className="flex flex-col items-center">
+        <div className="grid md:grid-cols-2 gap-4 mb-6">
+          {menu.map((item) => (
+            <MenuItem key={item._id} item={item}></MenuItem>
+          ))}
+        </div>
+        <button className="btn btn-outline border-0 border-b-4 mt-4">
+          View Full Menu
+        </button>
       </div>
     </section>
   );
